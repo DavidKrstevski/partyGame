e3e6f620066beacc7579f2a770b87a131fa925ca
David Krstevski <DavidKrstevski10@gmail.com>
2021-02-12 22:40:41 +0100

    First Testing

---
+++ KingsParty/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    [SerializeField]
+    private float speed;
+    void Update()
+    {
+        Move();
+    }
+
+
+    private void Move()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        gameObject.transform.position = new Vector2(transform.position.x + (h * speed),
+           transform.position.y + (v * speed));
+    }
+}
